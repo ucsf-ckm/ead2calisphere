c929666fccfb039a1e64f1825ef190ba47d2b7c9
Rich Trott <rtrott@gmail.com>
2020-11-24 14:19:30 -0800

    chore: add download button for web page

--- web/main.js
+++ web/main.js
@@ -19,10 +19,14 @@ function dragOverHandler (ev) {
 }
 
 const gallery = document.getElementById('gallery')
+const download = document.getElementById('download')
+let downloadMe
+let a
 
 function processFile (files) {
   gallery.innerHTML = ''
   const file = files[0]
+  const filename = file.name.replace(/.xml$/i, '')
   const reader = new window.FileReader()
   reader.readAsText(file)
   reader.onloadend = function () {
@@ -31,10 +35,24 @@ function processFile (files) {
     const pre = document.createElement('pre')
     const warnings = []
     pre.textContent = window.convert(reader.result, { warn: (txt) => { warnings.push(txt) } })
-    div.appendChild(pre)
-    gallery.appendChild(div)
     if (warnings.length) {
       window.alert(warnings.join('\n'))
     }
+    div.appendChild(pre)
+    gallery.appendChild(div)
+    const blob = new window.Blob([pre.textContent], { type: 'text/plain' })
+    URL.revokeObjectURL(downloadMe)
+    downloadMe = URL.createObjectURL(blob)
+
+    const newA = document.createElement('a')
+    newA.href = downloadMe
+    newA.download = `${filename}.tsv`
+    newA.innerHTML = `Download ${filename}.tsv`
+    if (a) {
+      a.replaceWith(newA)
+    } else {
+      download.appendChild(newA)
+    }
+    a = newA
   }
 }
